2901b60d66f0a7d1d8c1f878f71bd20dbdd2d5e4
roskomatt5 <142241842+roskomatt5@users.noreply.github.com>
2023-08-17 20:57:08 -0500

    Update prework_homework.py

--- prework_homework.py
+++ prework_homework.py
@@ -1,3 +1,4 @@
+#Question 1
 # username = input("What is your username? ")
 
 # def hello_user(user_name):
@@ -5,7 +6,7 @@
     
 # hello_user(username)
 ######## End of Question 1 Answer ^^^^^^^^##############
-
+#Question 2
 
 # def first_odds(start, end):
 #     for i in range(start,end + 1):
@@ -13,7 +14,7 @@
 #            return
 # first_odds(1,100)
 ######## End of Question 2 Answer ^^^^^^^^##############
-
+#Question 3
 # my_list = [1,2,3,4,5]
 
 # def max_num_in_list(a_list):
@@ -23,7 +24,7 @@
 # max_num_in_list(my_list)
 
 ######## End of Question 3 Answer ^^^^^^^^##############
-
+#Question 4
 
 # def is_a_leap_year(a_year):
 #     if a_year % 4 == 0 or a_year % 400 == 0 and a_year % 100 !=0:
@@ -34,7 +35,7 @@
 # is_a_leap_year(2016)
 
 # ######## End of Question 4 Answer ^^^^^^^^##############
-           
+#Question 5           
 # my_list = [1,2,3,4,6]
 
 # def is_consecutive(a_list):

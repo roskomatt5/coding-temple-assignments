4ad0ccdf0c8eedde361b107aa62772c3aad7f737
Matt Rosko <mattrosko@Matts-MacBook-Air.local>
2023-08-17 20:28:46 -0500

    Module 0 Questions

---
+++ prework_homework.py
@@ -0,0 +1,54 @@
+# username = input("What is your username? ")
+
+# def hello_user(user_name):
+#     print(f'hello_{user_name.upper()}')
+    
+# hello_user(username)
+######## End of Question 1 Answer ^^^^^^^^##############
+
+
+# def first_odds(start, end):
+#     for i in range(start,end + 1):
+#         if i % 2 != 0:
+#            return
+# first_odds(1,100)
+######## End of Question 2 Answer ^^^^^^^^##############
+
+# my_list = [1,2,3,4,5]
+
+# def max_num_in_list(a_list):
+#     a_list.sort()
+#     print(a_list[-1])
+
+# max_num_in_list(my_list)
+
+######## End of Question 3 Answer ^^^^^^^^##############
+
+
+# def is_a_leap_year(a_year):
+#     if a_year % 4 == 0 or a_year % 400 == 0 and a_year % 100 !=0:
+#         print(True)
+#     else:
+#         print(False)
+
+# is_a_leap_year(2016)
+
+# ######## End of Question 4 Answer ^^^^^^^^##############
+           
+# my_list = [1,2,3,4,6]
+
+# def is_consecutive(a_list):
+#     if sorted(a_list) == list(range(min(a_list),max(a_list)+1)):
+#         print(True)
+#     else:
+#         print(False)
+
+
+# is_consecutive(my_list)
+
+# # ######## End of Question 5 Answer ^^^^^^^^##############
+
+
+
+
+
